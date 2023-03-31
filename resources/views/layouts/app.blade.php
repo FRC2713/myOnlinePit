7441cc601dbeb6277b16ba3567831e4c15eb06e7
The Fake Slim Shady <74916637+Sid220@users.noreply.github.com>
2023-03-31 13:57:38 -0400

    Logoing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     @livewireStyles
     @vite('resources/css/app.css')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myOnlinePit</title>
 </head>
 
@@ -10,7 +11,9 @@
 <nav class="bg-white border-gray-200">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <a href="/" class="flex items-center">
-            <span class="self-center text-xl font-semibold whitespace-nowrap"><i>my</i>OnlinePit - {{ \App\Config::team() }}</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap">
+                <img src="{{ url("/img/logo.svg") }}"/>
+            </span>
         </a>
     </div>
 </nav>
@@ -19,7 +22,8 @@
         <div class="flex items-center">
             <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
                 <li>
-                    <a href="{{ route('nextmatch') }}" class="text-gray-900 hover:underline" aria-current="page">Pit List</a>
+                    <a href="{{ route('nextmatch') }}" class="text-gray-900 hover:underline" aria-current="page">Pit
+                        List</a>
                 </li>
                 <li>
                     <a href="{{ route('settings') }}" class="text-gray-900 hover:underline">Settings</a>
